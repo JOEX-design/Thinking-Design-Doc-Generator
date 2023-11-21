971784bfa02440438390814907dac153abe27f1b
JoeX <JoeX@local>
2023-11-21 16:10:30 +0800

    support multiple breakdown sections

--- src/extractor/extractBreakdown.ts
+++ src/extractor/extractBreakdown.ts
@@ -2,29 +2,33 @@ import {Buffer} from 'buffer';
 
 const extractBreakdown = async (frameNode: FrameNode) => {
 
-  const breakdownFrameList = frameNode.findAll(n => n.name === "breakdown-list") as FrameNode[]
-  const itemFrameList = breakdownFrameList[0].children as FrameNode[]
-  const frameImgShowcase = frameNode.findOne(n => n.name === "component-showcase") as FrameNode
+  const breakdownContentListFrames = frameNode.findAll(n => n.name === 'content') as FrameNode[]
+  const contentList = await Promise.all(breakdownContentListFrames.map(async listItem => {
+    const breakdownFrameList = listItem.findAll(n => n.name === "breakdown-list") as FrameNode[]
+    const itemFrameList = breakdownFrameList[0].children as FrameNode[]
+    const frameImgShowcase = listItem.findOne(n => n.name === "component-showcase") as FrameNode
+    const contentHeader = listItem.findOne(n => n.name === 'header') as TextNode
 
-  console.log("----DocGen-----: start extract breakdowns")
+    const imgBytes = await frameImgShowcase.exportAsync({ format: 'PNG', constraint: { type: 'SCALE', value: 2 } }).catch(e => console.log(e)) as Uint8Array
+    const imgBytesStr = Buffer.from(imgBytes).toString('base64')
 
-  const imgBytes = await frameImgShowcase.exportAsync({ format: 'PNG', constraint: { type: 'SCALE', value: 2 } }).catch(e => console.log(e)) as Uint8Array
-  const imgBytesStr = Buffer.from(imgBytes).toString('base64')
-
-  const breakdownList = itemFrameList.map((listItem, index) => {
-    const contents = listItem.findAll(n => n.type === "TEXT")
+    const breakdownList = itemFrameList.map((listItem, index) => {
+      const contents = listItem.findAll(n => n.type === "TEXT")
+      return {
+        key: index,
+        breakdownItem: contents[1]?.name,
+        breakdownValue: contents[2]?.name
+      }
+    })
     return {
-      key: index,
-      breakdownItem: contents[1].name,
-      breakdownValue: contents[2].name
+      breakdownImg: imgBytesStr,
+      breakdownList: breakdownList,
+      header: contentHeader?.characters
     }
-  })
+  }))
 
-  console.log("----DocGen-----: finished extract breakdowns")
-
-  return { 
-    breakdownImg: imgBytesStr,
-    breakdownList: breakdownList
+  return {
+    contents: contentList
   }
 }
 
--- src/ui/components/DocBreakdown.tsx
+++ src/ui/components/DocBreakdown.tsx
@@ -7,14 +7,22 @@ export const DocBreakdown = ({
   console.log('breakdown', breakdown)
   // const jsonData = code ? {...code} : null;
 
-  const breakdownItems = breakdown.breakdownList.map(item =>
-    <li key={item.key} className="flex items-start text-sm mb-3">
-        <div className="flex items-center mr-3">
-          <div className="rounded-full mr-2 w-4 h-4 text-center bg-purple-500 text-white text-xs leading-4">{item.key+1}</div>
-          <span className="text-purple-500">{item.breakdownItem}</span>
-        </div>
-        <span className="flex-1">{item.breakdownValue}</span>
-    </li>
+  const contentList = breakdown.contents.map ((content, index) =>
+    <div key={index}>
+      {content.header && <h4 className="mb-2 text-slate-600">{content.header}</h4>}
+      <ul>
+        {content.breakdownList.map(item =>
+          <li key={item.key} className="flex items-start text-sm mb-3">
+            <div className="flex items-center mr-3">
+              <div className="rounded-full mr-2 w-4 h-4 text-center bg-purple-500 text-white text-xs leading-4">{item.key+1}</div>
+              <span className="text-purple-500">{item.breakdownItem}</span>
+            </div>
+            <span className="flex-1">{item.breakdownValue}</span>
+          </li>        
+        )}
+      </ul>
+      <img className="w-full rounded-xl mb-12" src={generateImgSrc(content.breakdownImg)}></img>
+    </div>
   );
 
   return (
@@ -22,9 +30,8 @@ export const DocBreakdown = ({
       <div className="max-w-[850px] m-auto">
         <div className="pt-12 pb-8">
           <h2 id="组件解构" className="text-2xl text-slate-700 font-medium mb-5">组件解构</h2>
-          <ul>{breakdownItems}</ul>
         </div>
-        <img className="w-full rounded-xl mb-12" src={generateImgSrc(breakdown.breakdownImg)}></img>
+        {contentList}
       </div>
     </div>
   )
