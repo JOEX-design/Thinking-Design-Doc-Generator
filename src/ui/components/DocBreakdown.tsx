c5976fe771cb62254b655a3cf2d06756f84dd664
JoeX <JoeX@local>
2023-11-30 16:58:45 +0800

    v1.05: rewrite styles for DocBreakdown

--- src/ui/components/DocBreakdown.tsx
+++ src/ui/components/DocBreakdown.tsx
@@ -4,33 +4,28 @@ import generateImgSrc from "../../modules/generateImgSrc";
 export const DocBreakdown = ({
   breakdown,
 }) => {
-  console.log('breakdown', breakdown)
-  // const jsonData = code ? {...code} : null;
-
   const contentList = breakdown.contents.map ((content, index) =>
     <div key={index}>
-      {content.header && <h4 className="mb-2 text-slate-600">{content.header}</h4>}
+      {content.header && <h4>{content.header}</h4>}
       <ul>
         {content.breakdownList.map(item =>
-          <li key={item.key} className="flex items-start text-sm mb-3">
-            <div className="flex items-center mr-3">
-              <div className="rounded-full mr-2 w-4 h-4 text-center bg-purple-500 text-white text-xs leading-4">{item.key+1}</div>
-              <span className="text-purple-500">{item.breakdownItem}</span>
+          <li key={item.key} className="breakdown-item">
+            <div className="breakdown-item-header">
+              <div className="breakdown-item-dot">{item.key+1}</div>
+              <span>{item.breakdownItem}</span>
             </div>
-            <span className="flex-1">{item.breakdownValue}</span>
+            <span className="breakdown-item-value">{item.breakdownValue}</span>
           </li>        
         )}
       </ul>
-      <img className="w-full rounded-xl mb-12" src={generateImgSrc(content.breakdownImg)}></img>
+      <img className="component-showcase" src={generateImgSrc(content.breakdownImg)}></img>
     </div>
   );
 
   return (
-    <div className="border-b border-slate-200">
-      <div className="max-w-[850px] m-auto">
-        <div className="pt-12 pb-8">
-          <h2 id={breakdown.title || "组件解构"} className="text-2xl text-slate-700 font-medium mb-5">{breakdown.title || "组件解构"}</h2>
-        </div>
+    <div className="doc-section-wrapper">
+      <div className="doc-section">
+        <h2 id={breakdown.title || "组件解构"}>{breakdown.title || "组件解构"}</h2>
         {contentList}
       </div>
     </div>
