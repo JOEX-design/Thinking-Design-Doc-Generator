801a7e89b6486ae2d7ab5b972a3503b7ac20b418
JoeX <JoeX@local>
2023-12-04 17:19:07 +0800

    v1.05: rewrite styles for PatternStart & PatternEnd

--- src/ui/components/DocPatternEnd.tsx
+++ src/ui/components/DocPatternEnd.tsx
@@ -4,30 +4,20 @@ export const DocPatternEnd = ({
   patternEnd,
 }) => {
     const contentList = patternEnd.typeList.map((item, itemIndex) => 
-        <li key={itemIndex}>
-          <div className="flex gap-4 mb-2">
-            <div id={item.header.replace(/\s/g, '')} className="text-lg text-slate-700 font-medium">{item.header}</div>
-          </div>
-          <div className='text-sm'>
-            <ul className="list-disc ml-4">{
-              item.bulletPointList?.map((bullet, bulletIndex) =>
-                <li key={bulletIndex} className="mb-2">{bullet}</li>
-              )
-            }</ul>
-          </div>
-        </li>
+      <li key={itemIndex}>
+        <div id={item.header.replace(/\s/g, '')} className="pattern-header">{item.header}</div>
+        <ul className="content-desc">{
+          item.bulletPointList?.map((bullet, bulletIndex) =>
+            <li key={bulletIndex} className="bullet">{bullet}</li>
+          )
+        }</ul>
+      </li>
     )
 
     return (
-      <div className="border-t border-slate-200">
-        <div className="max-w-[850px] m-auto">
-          {/* <div className="py-12"> */}
-            {/* <h1 id={`compoName_${patternStart.title.replace(/\s/g, '')}`} className="text-3xl text-slate-900 font-medium mb-4">{patternStart.title}</h1> */}
-            {/* <h1 className="text-3xl text-slate-900 font-medium mb-4">{patternStart.title}</h1> */}
-          {/* </div> */}
-          <div className="py-10 w-3/4">
-            <ul className="flex flex-col gap-10">{contentList}</ul>
-          </div>
+      <div className="doc-section-wrapper">
+        <div className="doc-section">
+          <ul className="type-list">{contentList}</ul>
         </div>
       </div>
   )
