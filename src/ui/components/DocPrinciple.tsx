e2223038d8db40efeedbb071661859a67cbf3b01
JoeX <JoeX@local>
2023-12-04 13:58:16 +0800

    v1.05: rewrite styles for DocPrinciple

--- src/ui/components/DocPrinciple.tsx
+++ src/ui/components/DocPrinciple.tsx
@@ -1,34 +1,28 @@
 import * as React from "react";
 import generateImgSrc from "../../modules/generateImgSrc";
 
+const dotContent = (colorType) => {
+  if (colorType === 'green') return '✓';
+  else if (colorType === 'red') return '✗';
+  else if (colorType === 'orange') return '!';
+  else return '';
+}
+
+
 export const DocPrinciple = ({
   principle,
 }) => {
   const principleItem = principle.map(row =>
-    <div key={row.rowId} className="flex gap-10 items-start mb-20">
+    <div key={row.rowId} className="principle-row">
       {row.item.map(item => {
         return (
-          <div className={row.length==2 ? `w-1/2` : `w-auto`} key={item.itemId}>
-            <img className="w-full rounded-xl mb-9" src={generateImgSrc(item.img)}></img>
-            {item.type === 'green' && (
-              <div className="flex items-center mb-2">
-                <div className="rounded-full mr-2 w-5 h-5 text-center bg-green-700 text-white text-xs leading-5">✓</div>
-                <div className={` text-green-700`}>{item.title}</div>
+          <div key={item.itemId}  className={`principle-item ${row.length==2 ? "half" : "full"}`}>
+            <img src={generateImgSrc(item.img)}></img>
+              <div className="principle-item-content">
+                <div className={`principle-dot ${item.type}`}>{dotContent(item.type)}</div>
+                <div className={`principle-dot-desc ${item.type}`}>{item.title}</div>
               </div>
-              )}
-            {item.type === 'red' && (
-              <div className="flex items-center mb-2">
-                <div className="rounded-full mr-2 w-5 h-5 text-center bg-red-500 text-white text-xs leading-5">✗</div>
-                <div className={` text-red-500`}>{item.title}</div>
-              </div>
-            )}
-            {item.type === 'orange' && (
-              <div className="flex items-center mb-2">
-                <div className="rounded-full mr-2 w-5 h-5 text-center bg-orange-500 text-white text-xs leading-5">!</div>
-                <div className={` text-orange-500`}>{item.title}</div>
-              </div>
-            )}
-            <div className='text-sm'>{item.desc}</div>
+            <p>{item.desc}</p>
           </div>
         )
       })}
@@ -36,15 +30,10 @@ export const DocPrinciple = ({
   );
 
   return (
-    <div className="border-b border-slate-200">
-      <div className="max-w-[850px] m-auto">
-        <div className="py-12">
-          <h2 id="使用原则" className="text-2xl text-slate-700 font-medium mb-5">使用原则</h2>
-          <div className="py-10">
-            {principleItem}
-          </div>
-        </div>
-
+    <div className="doc-section-wrapper">
+      <div className="doc-section">
+        <h2 id="使用原则">使用原则</h2>
+        <div className="type-list">{principleItem}</div>
       </div>
     </div>
   )
